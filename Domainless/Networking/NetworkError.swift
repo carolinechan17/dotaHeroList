b7ad9287cf306a5df52b52c1d32a9fd6302b27ce
Caroline Chan <carolinechan304@gmail.com>
2022-10-03 20:18:47 +0700

    Create networking for dota hero list

---
+++ Domainless/Networking/NetworkError.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkError.swift
+//  dotaHeroList
+//
+//  Created by Caroline Chan on 03/10/22.
+//
+
+import Foundation
+
+enum NetworkError: Error, LocalizedError {
+
+    case middlewareError(code: Int, message: String?)
+    
+    var localizedDescription: String {
+        switch self {
+        case .middlewareError(_, let message):
+            return message ?? ""
+        }
+    }
+}
+
+struct NetworkHandle: Decodable, Error, LocalizedError {
+    let success: Bool
+    let data: NetworkHandleData
+    let message: String
+    let code: Int
+    let codeName: String
+
+    enum CodingKeys: String, CodingKey {
+        case success, data, message, code
+        case codeName = "code_name"
+    }
+}
+
+// MARK: - DataClass
+struct NetworkHandleData: Decodable {
+}
+
