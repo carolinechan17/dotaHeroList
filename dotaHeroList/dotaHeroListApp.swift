8387793f233dfa00f543449094a612abab74c53f
Caroline Chan <carolinechan304@gmail.com>
2022-10-03 20:11:43 +0700

    Initial commit

---
+++ dotaHeroList/dotaHeroListApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotaHeroListApp.swift
+//  dotaHeroList
+//
+//  Created by Caroline Chan on 03/10/22.
+//
+
+import SwiftUI
+
+@main
+struct dotaHeroListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
